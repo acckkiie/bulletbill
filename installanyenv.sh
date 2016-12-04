44af573f7fb56685856b64c20b04a66ac76ebe7c
4885rhkA <sun.oon.bow.reverse7@gmail.com>
2016-12-04 22:14:38 +0900

    add anyenv setting

---
+++ installanyenv.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Clone files for anyenv
+    git clone https://github.com/riywo/anyenv ~/.anyenv
+
+# Set .bash_profile
+    echo "export PATH=\$HOME/.anyenv/bin:\$PATH" >> .bash_profile
+    echo "eval '\$(anyenv init -)'" >> .bash_profile
+
+# reload
+    source .bash_profile
+
+# Clone files for anyenv-update
+    mkdir -p $(anyenv root)/plugins
+    git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+
+# Install environment control
+    anyenv install rbenv
+    anyenv install pyenv
+    anyenv install phpenv
+    anyenv install ndenv
+    anyenv install jenv
+    anyenv install goenv
+
+# Reload
+    source .bash_profile
+
+# http://qiita.com/jyotti/items/48115f54ffbf6dfdf6f2
+    jenv add `/usr/libexec/java_home -v 1.7`
+    jenv add `/usr/libexec/java_home -v 1.6`
+
+# Reload
+    source .bash_profile
