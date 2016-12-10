49c3b56cc0475121a4ce20fbc9f5fef35531aa7b
4885rhkA <sun.oon.bow.reverse7@gmail.com>
2016-12-10 11:38:27 +0900

    change the position about the setting without brew

--- main.sh
+++ main.sh
@@ -4,30 +4,46 @@ set -eu
 cd ~
 
 # Check bash files
-if [ -f .bash_profile ]; then
-    echo "Use existed .bash_profile"
-else
-    touch .bash_profile
-    chmod 644 .bash_profile
-    echo "Created .bash_profile"
-fi
+    if [ -f .bash_profile ]; then
+        echo "Use existed .bash_profile"
+    else
+        touch .bash_profile
+        chmod 644 .bash_profile
+        echo "Created .bash_profile"
+    fi
 
-if [ -f .bashrc ]; then
-    echo "Use existed .bashrc" 
-else
-    touch .bashrc
-    chmod 644 .bashrc
-    echo "Created .bashrc"
-fi
+    if [ -f .bashrc ]; then
+        echo "Use existed .bashrc" 
+    else
+        touch .bashrc
+        chmod 644 .bashrc
+        echo "Created .bashrc"
+    fi
+
+# Check alerady file existed
+    if [ -d /usr/local/include ]; then
+        echo "Use existed /usr/local/include" 
+    else
+        mkdir /usr/local/include
+        chmod 755 /usr/local/include
+        echo "Created /usr/local/include"
+    fi
+
+# Check environment automatically for Installing Homebrew
+    xcode-select --install
+
+# Install Homebrew
+# http://qiita.com/oooaoii/items/c14922eede6a83a750da
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install Homebrew and commands
-./installbrew.sh
+    ./installbrew.sh
 
 # Install applications by homebrew-cask
-./installapps.sh
+    ./installapps.sh
 
 # Install anyenv
-./installanyenv.sh
+    ./installanyenv.sh
 
 # Set configration
-./setbash.sh $1 $2
+    ./setbash.sh $1 $2
