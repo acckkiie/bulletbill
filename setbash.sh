f485f7ea7c1b285834d2cc05acd969f422c8ffa3
4885rhkA <sun.oon.bow.reverse7@gmail.com>
2016-12-13 22:10:15 +0900

    add for setting correct directory

--- setbash.sh
+++ setbash.sh
@@ -5,25 +5,27 @@ email=$2
 
 # .bash_profile setting
 # http://qiita.com/eumesy/items/3bb39fc783c8d4863c5f#coreutils-用に特別な-path-を通す理由
-echo ". \$(brew --prefix)/etc/bash_completion" >> .bash_profile
+echo ". \$(brew --prefix)/etc/bash_completion" >> ~/.bash_profile
 
 # http://d.hatena.ne.jp/katz_24/20120126/1327586474
 echo "/usr/local/bin:\$PATH" >> ~./bash_profile
-echo "export PATH=\$(brew --prefix coreutils)/libexec/gnubin:\$PATH" >> .bash_profile
-echo "export MANPATH=\$(brew --prefix coreutils)/libexec/gnuman:\$MANPATH" >> .bash_profile
+echo "export PATH=\$(brew --prefix coreutils)/libexec/gnubin:\$PATH" >> ~/.bash_profile
+echo "export MANPATH=\$(brew --prefix coreutils)/libexec/gnuman:\$MANPATH" >> ~/.bash_profile
 
 # .bashrc setting
-echo "alias diff='colordiff'" >> .bashrc
-echo "alias ls='ls --color=auto'" >> .bashrc
-echo "alias awk='gawk'" >> .bashrc
-echo "alias rm='rmtrash'" >> .bashrc
+echo "alias diff='colordiff'" >> ~/.bashrc
+echo "alias ls='ls --color=auto'" >> ~/.bashrc
+echo "alias awk='gawk'" >> ~/.bashrc
+echo "alias rm='rmtrash'" >> ~/.bashrc
 
 # reload
-source .bash_profile
-source .bashrc
+source ~/.bash_profile
+source ~/.bashrc
 
 git config --global user.name $name
 git config --global user.email $email
+
+# alfred アクセシビリティ
 
 # font
 # http://qiita.com/muran001/items/409b8cb42cca02b147a0#tmuxを入れてクリップボード共有もついでにやっておく
