7ac798f0feeb2a9fbb6c58fe6dbdcf5d8346e93d
4885rhkA <sun.oon.bow.reverse7@gmail.com>
2016-12-13 22:10:43 +0900

    set correct directory

--- main.sh
+++ main.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 set -eu
-password=$1
-name=$2
-email=$3
 
-cd ~
+echo -n "Password:"
+read -rs password
+echo
+echo -n "\nGit Name:"
+read -r name
+echo -n "Git Email:"
+read -r email
 
 # Check bash files
-if [ -f .bash_profile ]; then
-    echo "Use existed .bash_profile"
+if [ -f ~/.bash_profile ]; then
+    echo "Use existed ~/.bash_profile"
 else
-    touch .bash_profile
-    ./autoreply.sh "sudo chmod 644 .bash_profile" $password
-    echo "Created .bash_profile"
+    touch ~/.bash_profile
+    bulletbill/autoreply.sh "sudo chmod 644 ~/.bash_profile" $password
+    echo "Created ~/.bash_profile"
 fi
 
-if [ -f .bashrc ]; then
-    echo "Use existed .bashrc" 
+if [ -f ~/.bashrc ]; then
+    echo "Use existed ~/.bashrc" 
 else
-    touch .bashrc
-    ./autoreply.sh "sudo chmod 644 .bashrc" $password
-    echo "Created .bashrc"
+    touch ~/.bashrc
+    bulletbill/autoreply.sh "sudo chmod 644 ~/.bashrc" $password
+    echo "Created ~/.bashrc"
 fi
 
 # Check alerady file existed
 if [ -d /usr/local/include ]; then
     echo "Use existed /usr/local/include" 
 else
-    ./autoreply.sh "sudo mkdir /usr/local/include" $password
-    ./autoreply.sh "sudo chmod 755 /usr/local/include" $password
+    bulletbill/autoreply.sh "sudo mkdir /usr/local/include" $password
+    bulletbill/autoreply.sh "sudo chmod 755 /usr/local/include" $password
     echo "Created /usr/local/include"
 fi
 
 # Install Homebrew
 # http://qiita.com/oooaoii/items/c14922eede6a83a750da
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install > installhomebrew.rb
-./autoreply.sh "ruby installhomebrew.rb" $password
-rm -rf installhomebrew.rb
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install > bulletbill/installhomebrew.rb
+bulletbill/autoreply.sh "ruby bulletbill/installhomebrew.rb" $password
+rm -rf bulletbill/installhomebrew.rb
 
 # Add repository
-cat Brewrepository | while read line
+cat bulletbill/Brewrepository | while read line
 do
     if ! echo "$line" | grep -sq "#"; then
         brew tap "$line"
@@ -47,10 +50,10 @@ do
 done
 
 # Install Homebrew and commands
-cat Brewfile | while read line
+cat bulletbill/Brewfile | while read line
 do
     if ! echo "$line" | grep -sq "#"; then
-        ./autoreply.sh "brew install ${line}" $password
+        bulletbill/autoreply.sh "brew install ${line}" $password
     fi
 done
 
@@ -61,30 +64,26 @@ brew linkapps
 brew cleanup
 
 # Install applications by homebrew-cask
-cat Brewcaskfile | while read line
+cat bulletbill/Brewcaskfile | while read line
 do
     if ! echo "$line" | grep -sq "#"; then
-        ./autoreply.sh "brew cask install ${line}" $password
+        bulletbill/autoreply.sh "brew cask install ${line}" $password
     fi
 done
-
-# link
-# brew-caskのappをalfredで探せるようにする
-brew cask alfred link
 
 # Cleanup .dmg
 brew cask cleanup
 
 # Install Applications using mas fron AppStore
-cat Masfile | while read line
+cat bulletbill/Masfile | while read line
 do
     if ! echo "$line" | grep -sq "#"; then
-        ./autoreply.sh "mas install ${line}" $password
+        bulletbill/autoreply.sh "mas install ${line}" $password
     fi
 done
 
 # Install anyenv
-./installanyenv.sh
+bulletbill/installanyenv.sh
 
 # Set configration
-./setbash.sh $name $mail
+bulletbill/setbash.sh $name $mail
