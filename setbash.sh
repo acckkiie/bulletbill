a0c71a624ee0f2b977950217362d0770ebda1f97
4885rhkA <sun.oon.bow.reverse7@gmail.com>
2016-12-23 17:34:39 +0900

    add bash function

--- setbash.sh
+++ setbash.sh
@@ -3,6 +3,16 @@ set -eu
 gitname=$1
 gitemail=$2
 
+# http://qiita.com/key-amb/items/d053ed80590697435650
+./autoreply.sh "echo '/usr/local/bin/bash' | sudo tee -a /etc/shells"
+chsh -s /usr/local/bin/bash
+
+# bash setting
+shopt -s autocd
+shopt -s cdspell
+shopt -s dotglob
+shopt -s extglob
+
 # http://d.hatena.ne.jp/katz_24/20120126/1327586474
 echo "/usr/local/bin:\$PATH" >> ~./bash_profile
 echo "export PATH=\$(brew --prefix coreutils)/libexec/gnubin:\$PATH" >> ~/.bash_profile
@@ -13,6 +23,13 @@ echo "alias diff='colordiff'" >> ~/.bashrc
 echo "alias ls='ls --color=auto'" >> ~/.bashrc
 echo "alias awk='gawk'" >> ~/.bashrc
 echo "alias rm='rmtrash'" >> ~/.bashrc
+echo "alias tree='tree -N'" >> ~/.bashrc
+
+# load .bashrc automatically
+echo "source ~/.bashrc" >> ~/.bash_profile
+
+# change view of bash
+"export PS1='\[\e[1;34m\][\d \t \u@\h \W]\$\[\e[0m\] '" >> ~/.bash_profile
 
 # reload
 source ~/.bash_profile
